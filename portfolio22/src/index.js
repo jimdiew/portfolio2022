eeb7f9c46ba7f2bda7a00915d9c37bb211633918
Jimena Diewiatajew <39556832+jimdiew@users.noreply.github.com>
2022-02-28 01:28:34 -0300

    add routing

---
+++ portfolio22/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/css/index.css";
+import App from "./components/routes/App.js";
+
+ReactDOM.render(
+  <React.Fragment>
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);

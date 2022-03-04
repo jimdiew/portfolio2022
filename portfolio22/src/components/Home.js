5726b0e1d12b0f4122d7a73db8269dbe80909271
Jimena Diewiatajew <39556832+jimdiew@users.noreply.github.com>
2022-03-04 09:48:31 -0300

    update

--- portfolio22/src/components/Home.js
+++ portfolio22/src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Container } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 
 export default function Home() {
   return (
     <Container>
-      <h1 className="play-font">Hi!</h1>
-      <p>I'm a front end developer</p>
+      <Box>
+        <h1 className="play-font">Hi!</h1>
+        <p>I'm a front end developer</p>
+      </Box>
     </Container>
   );
 }

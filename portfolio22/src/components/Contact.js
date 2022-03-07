7a4ef2259407db250fce5b919428ee41beef6429
Jimena Diewiatajew <jimediew@gmail.com>
2022-03-07 14:15:05 -0300

    add text fields and button

--- portfolio22/src/components/Contact.js
+++ portfolio22/src/components/Contact.js
@@ -1,10 +1,35 @@
 import React from "react";
 import Container from "@mui/material/Container";
+import { TextField, Button, Typography } from "@mui/material";
 
 export default function Contact() {
   return (
     <Container>
-      <h1 className="play-font">Contact</h1>
+      <Typography variant="h1" color="white" align="center">
+        Contact
+      </Typography>
+      <TextField
+        id="filled-basic"
+        label="Name"
+        variant="filled"
+        sx={{ m: 3 }}
+      />
+      <TextField
+        id="filled-basic"
+        label="E-mail"
+        variant="filled"
+        sx={{ m: 3 }}
+      />
+      <TextField
+        id="filled-textarea"
+        label="Message"
+        multiline
+        variant="filled"
+        sx={{ m: 3 }}
+      />
+      <Button variant="contained" color="primary" disableElevation>
+        SEND
+      </Button>
     </Container>
   );
 }

274eb0c24955e468d7762d40027c6a1fcc72b3ff
Jimena Diewiatajew <jimediew@gmail.com>
2022-03-07 14:22:01 -0300

    add themeprovider

--- portfolio22/src/components/routes/App.js
+++ portfolio22/src/components/routes/App.js
@@ -4,18 +4,22 @@ import Home from "../Home";
 import Education from "../Education";
 import Contact from "../Contact";
 import Layout from "../Layout";
+import { ThemeProvider } from "@mui/material";
+import theme from "../../assets/theme.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact element={<Home />} path="/"></Route>
-          <Route exact element={<Education />} path="/education"></Route>
-          <Route exact element={<Contact />} path="contact/"></Route>
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact element={<Home />} path="/"></Route>
+            <Route exact element={<Education />} path="/education"></Route>
+            <Route exact element={<Contact />} path="contact/"></Route>
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 

4a28fa93ccc5c079c7b1d0e4483d53437f160312
Jimena Diewiatajew <jimediew@gmail.com>
2022-10-06 12:26:11 -0300

    Create footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './footer.css'
+import {RiInstagramFill} from 'react-icons/ri'
+import {FaLinkedin} from 'react-icons/fa'
+import {BsGithub} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <footer>
+    <a href="#" className='footer__logo'>JIMDIEW</a>
+    
+      <div className='footer__socials'>
+      <a href="https://www.linkedin.com/in/jimdiew/"><FaLinkedin/></a>
+      <a href="https://github.com/jimdiew"><BsGithub/></a>
+      <a href="https://www.instagram.com/watch.jim.code/"><RiInstagramFill/></a>
+      </div>
+   
+
+    <div className="footer__copyright">
+      <small>&copy; Jimena Diewiatajew. All rights reserved.</small>
+    </div>
+   </footer>
   )
 }
 
